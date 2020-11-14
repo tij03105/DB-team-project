e9fb3125d033997e2ccd5867bd7d3b897b7c8c09
tij03105 <tij03105@gmail.com>
2020-11-14 18:26:12 +0900

    Update admin.java

--- movie/src/admin.java
+++ movie/src/admin.java
@@ -1,19 +1,19 @@
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Scanner;
+import java.util.*;
+import java.util.Date;
 
 public class admin {
+	private static Scanner sc = new Scanner(System.in);
+
     public static void upload_movie(Connection conn, Statement stmt){
     	if(account.ADMIN == false) System.out.println("잘못된 접근 : 관리자가 아닙니다");
 		else {
 			try {
 				String r_id;
 				String input;
-				Scanner sc = new Scanner(System.in);
+
 				/*
 				sql = "select min(tconst) from movie";
 				ResultSet rs = stmt.executeQuery(sql);
@@ -124,6 +124,122 @@ public class admin {
 				System.exit(1);
 			}
 		
+		}
+	}
+
+	public static void updateMovie(Connection conn, Statement stmt){
+		System.out.println("영상 수정을 선택하셨습니다.");
+		while(true){
+			System.out.println("수정할 영상의 이름을 입력하세요.");
+			String name = "";
+			name = sc.nextLine();
+
+			try {
+				String sql = "SELECT tconst, start_year FROM MOVIE WHERE title = '" + name + "'";
+				ResultSet rs = stmt.executeQuery(sql);
+				ResultSetMetaData resultSetMetaData = rs.getMetaData();
+
+				//System.out.println(sql);
+				if(!rs.isBeforeFirst()) {
+					System.out.println("일치하는 영상이 없습니다.");
+					System.out.println("다른 영상을 찾으시겠습니까?(Y:yes, N:no)");
+					String q;
+					q = sc.nextLine();
+					if (q.equals("y") || q.equals("Y") || q.equals("yes") || q.equals("YES")) continue;
+					else if(q.equals("n") || q.equals("N") || q.equals("no") || q.equals("NO")) break;
+					else {
+						System.out.println("잘못된 값입니다. 이전메뉴로 돌아갑니다.");
+						break;
+					}
+				}
+
+				String tconst;
+				Date date;
+				System.out.println("입력한 영상의 이름과 일치하는 목록");
+				int cnt = resultSetMetaData.getColumnCount();
+				System.out.printf("    ");
+				for(int i=1;i<=cnt;i++){
+					System.out.printf(resultSetMetaData.getColumnName(i));
+					if(i!=cnt) System.out.printf("			");
+					else System.out.println("			" + " TITLE");
+				}
+
+				cnt = 1;
+				ArrayList<String> id_list = new ArrayList<>();
+				while (rs.next()) {
+					tconst = rs.getString(1);
+					date = rs.getDate(2);
+					System.out.println(cnt + "  " + tconst + "		" + date + "			" + name);
+					cnt++;
+					id_list.add(tconst);
+				}
+
+				System.out.println("수정할 영상를 선택하세요");
+				try{
+					int idx = sc.nextInt();
+					sc.nextLine();
+					tconst = id_list.get(idx-1);
+				} catch (InputMismatchException e){
+					System.out.println("잘못된 입력입니다. 이전메뉴로 돌아갑니다.");
+					break;
+				} catch (IndexOutOfBoundsException e){
+					System.out.println("잘못된 입력입니다. 이전메뉴로 돌아갑니다.");
+					break;
+				}
+
+				String title, titile_type, adult, start_year;
+				int runtime, gCode;
+				System.out.println("수정을 원하지 않는 정보는 비워두세요.");
+
+				System.out.print("Title : ");
+				title = sc.nextLine();
+				System.out.print("Title type : ");
+				titile_type = sc.nextLine();
+				while (true) {
+					System.out.print("Adult(18세 이상: A, 미만: R) : ");
+					adult = sc.nextLine();
+					if (adult.equals("A") || adult.equals("R") || adult.equals("")) break;
+					else System.out.println("A또는 R만 입력가능합니다.");
+				}
+
+				while (true) {
+					System.out.print("Start Year(YYYYMMDD) : ");
+					start_year = sc.nextLine();
+					if (start_year.equals("") || start_year.matches("[0-9]*") && start_year.length() == 8) break;
+					else System.out.println("입력 형식을 다시 확인하세요.");
+				}
+
+				System.out.print("Run time : ");
+				runtime = sc.nextInt();
+
+				System.out.println("아래의 장르중 선택하세요.");
+				System.out.println();
+				sql = "UPDATE MOVIE SET ";
+				String setSql = "";
+				/*
+				if(!phone.equals("")) setSql += "phone = '" + phone + "', ";
+				if(!name.equals("")) setSql += "name = '" + name + "', ";
+				if(!address.equals("")) setSql += "address = '" + address + "', ";
+				if(sex.equals("0")) setSql += "sex = 'M', ";
+				else if(sex.equals("1")) setSql += "sex = 'F', ";
+				if(!date.equals("")) setSql += "bdate = TO_DATE('" + date.substring(0,4) + "-" + date.substring(4,6) + "-" + date.substring(6) + "', 'yyyy-mm-dd'), ";
+				if(!job.equals("")) setSql += "job = '" + job + "', ";
+				setSql = setSql.substring(0, setSql.length()-2);
+				sql += setSql + " WHERE id = '" + ID + "'";
+				//System.out.println(sql);
+				 */
+				if(!setSql.equals("")) {
+					stmt.executeUpdate(sql);
+					conn.commit();
+					System.out.println("회원정보가 수정되었습니다.");
+				}
+				else{
+					System.out.println("변경할 정보가 없습니다.");
+				}
+			} catch(SQLException ex) {
+				System.err.println("sql error = " + ex.getMessage());
+				System.exit(1);
+			}
 		}
 	}
 }
